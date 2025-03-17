bbe020d2bb1b9a011be5e8ff33373566a5615362
tenmonster <tenmonster@hotmail.com>
2025-03-17 16:28:03 +0700

    Edit Import

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Teneieiza/Golang-basic/defer"
+	// "github.com/Teneieiza/Golang-basic/map"
+	// "github.com/Teneieiza/Golang-basic/channel"
+	// "github.com/Teneieiza/Golang-basic/interface"
+	// "github.com/Teneieiza/Golang-basic/struct"
+	// "github.com/Teneieiza/Golang-basic/workshop"
+)
 
 func main() {
 	fmt.Println("Hello, World! with Go Lang eieiza")
+
+	// workshop.Calculator()
+	// structexam.Struct()  				// แสดงข้อมูลจาก struct.go
+	// mapexam.Map()				 				// แสดงข้อมูลจาก map.go
+	// interfaceexam.Interface() 	// แสดงข้อมูลจาก interface.go
+	// channel.Routine()						// แสดงข้อมูลจาก goroutines.go
+	// channel.Channel()						// แสดงข้อมูลจาก channel.go
+	deferexam.Defer()						// แสดงข้อมูลจาก defer.go
 }
